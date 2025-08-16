28a6b893511bd656742fcd9ae96675916736a108
bdmtnz <martinez_o@hotmail.es>
2025-08-16 14:40:55 -0500

    feat: advanced changes

--- million-frontend/src/pages/property/pages/DetailProperty.tsx
+++ million-frontend/src/pages/property/pages/DetailProperty.tsx
@@ -21,50 +21,54 @@ const DetailProperty = () => {
     
     return (
         <>
-            <div className="relative w-full h-60">
+            <div className="relative w-full h-70">
                 <img className="absolute inset-0 w-full h-full object-cover opacity-50" src={data.image} alt="Fondo borroso"/>
                 <div className="relative top-0 left-0 w-full h-full backdrop-blur-xs flex items-center justify-center">
-                    <div className="relative w-60 aspect-square">
+                    <div className="relative w-70 aspect-square">
                         <img className="w-full h-full object-cover" src={data.image} alt='property-img' />
                     </div>
                 </div>
-                <div className="absolute bottom-4 left-0 px-10 sm:px-20">
-                    <h1 className="text-2xl font-bold">{data.name}</h1>
+                <div className="relative w-full h-10 left-0 -top-10 flex justify-center">
+                    <div className="w-full max-w-7xl px-10 sm:px-20">
+                        <h1 className="text-2xl font-bold">{data.name}</h1>
+                    </div>
                 </div>
             </div>
-            <div className="px-10 sm:px-20 max-w-7xl">
-                <div className="flex flex-col gap-10">
-                    <div>
-                        <h2 className="text-xl font-semibold border-b-2 border-gray-200 pb-2 mb-4">Property details</h2>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                            <div>
-                                <p className="text-gray-300">Address:</p>
-                                <p className="font-medium text-gray-50">Address 4</p>
+            <div className="w-full flex justify-center">
+                <div className="px-10 sm:px-20 w-full max-w-7xl">
+                    <div className="flex flex-col gap-10">
+                        <div>
+                            <h2 className="text-xl font-semibold border-b-2 border-gray-200 pb-2 mb-4">Property details</h2>
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                <div>
+                                    <p className="text-gray-300">Address:</p>
+                                    <p className="font-medium text-gray-50">Address 4</p>
+                                </div>
+                                <div>
+                                    <p className="text-gray-300">Builded On:</p>
+                                    <p className="font-medium text-gray-50">2003</p>
+                                </div>
+                                <div className="col-span-1 md:col-span-2">
+                                    <p className="text-gray-300">Price:</p>
+                                    <p className="text-3xl font-bold text-emerald-600">{Utils.formatCurrency(data.price)}</p>
+                                </div>
                             </div>
-                            <div>
-                                <p className="text-gray-300">Builded On:</p>
-                                <p className="font-medium text-gray-50">2003</p>
-                            </div>
-                            <div className="col-span-1 md:col-span-2">
-                                <p className="text-gray-300">Price:</p>
-                                <p className="text-3xl font-bold text-emerald-600">{Utils.formatCurrency(data.price)}</p>
+                            <div className="mt-4 text-sm text-gray-400">
+                                <p>ID: {data.id}</p>
+                                <p>Code: {data.code}</p>
                             </div>
                         </div>
-                        <div className="mt-4 text-sm text-gray-400">
-                            <p>ID: {data.id}</p>
-                            <p>Code: {data.code}</p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 className="text-xl font-semibold border-b-2 border-gray-200 pb-2 mb-4">Owner details</h2>
-                        <div className="flex gap-8">
-                            <div className="flex items-center space-x-4">
-                                <img className="w-16 h-16 rounded-full object-cover border-2 border-gray-300" src={data.owner.photo} alt="Owner 4"/>
-                            </div>
-                            <div>
-                                <p className="text-lg font-medium text-gray-50">{data.owner.name}</p>
-                                <p className="text-sm text-gray-300">{data.owner.address}</p>
-                                <p className="text-sm text-gray-300">Born On: {Utils.formatDate(data.owner.bornOnUtc)}</p>
+                        <div>
+                            <h2 className="text-xl font-semibold border-b-2 border-gray-200 pb-2 mb-4">Owner details</h2>
+                            <div className="flex gap-8">
+                                <div className="flex items-center space-x-4">
+                                    <img className="w-16 h-16 rounded-full object-cover border-2 border-gray-300" src={data.owner.photo} alt="Owner 4"/>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-medium text-gray-50">{data.owner.name}</p>
+                                    <p className="text-sm text-gray-300">{data.owner.address}</p>
+                                    <p className="text-sm text-gray-300">Born On: {Utils.formatDate(data.owner.bornOnUtc)}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
