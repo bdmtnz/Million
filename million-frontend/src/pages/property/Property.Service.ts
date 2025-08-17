9420d44c53831c4633bf5026e53809a92546b3e8
bdmtnz <martinez_o@hotmail.es>
2025-08-17 00:50:54 -0500

    feat: adavnced changes

--- million-frontend/src/pages/property/Property.Service.ts
+++ million-frontend/src/pages/property/Property.Service.ts
@@ -2,10 +2,16 @@ import axios from "axios";
 import type { Property, PropertyFiltered } from "./Property.Model";
 import type { Pagination } from "../../shared/models/Responses";
 
-const baseURL = "https://localhost:7257";
+const baseURL = import.meta.env.VITE_API_URL;
 
 export const PropertyService = {
-    get: (keyword: string | null = null, limit: number = 5, offset: number = 1, from: number | null = null, to: number | null = null) => {
+    get: (
+        keyword: string | null = null, 
+        limit: number = 5, 
+        offset: number = 1, 
+        from: number | null = null, 
+        to: number | null = null
+    ) => {
         return axios.get<Pagination<PropertyFiltered>>(`${baseURL}/api/Property?keyword=${keyword??''}&limit=${limit??''}&offset=${offset??''}&from=${from??''}&to=${to??''}`)
     },
     getById: (id: string) => axios.get<Property>(`${baseURL}/api/Property/${id}`)
